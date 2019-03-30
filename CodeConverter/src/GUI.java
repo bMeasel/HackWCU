ee80e4efc9904c3a6de389362f8542ac93da4277
Xonor13 <xonor1@gmail.com>
2019-03-30 16:07:37 -0400

    Update GUI.java

--- CodeConverter/src/GUI.java
+++ CodeConverter/src/GUI.java
@@ -1,5 +1,30 @@
+import java.awt.Frame;
+import javax.swing.*;
 
-public class GUI 
+public class GUI extends Frame
 {
+	public JFrame frame;
+	public JPanel mainPanel;
+	public JButton convertBtn;
+	public JComboBox dropDownListLeft, dropDownListRight;
 	
+	public GUI()
+	{
+		//Panels and Frame
+		frame = new JFrame("Code Converter");
+		mainPanel = new JPanel();
+		frame.add(mainPanel);
+		
+		//Buttons
+		convertBtn = new JButton("Convert");
+		mainPanel.add(convertBtn);
+		
+		//ComboBox
+		dropDownListLeft = new JComboBox();
+		dropDownListRight = new JComboBox();
+		mainPanel.add(dropDownListLeft);
+		mainPanel.add(dropDownListRight);
+		
+		frame.setVisible(true);
+	}
 }
