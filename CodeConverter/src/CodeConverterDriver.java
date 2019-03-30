fd2a3ffbebb1fc0b5790b3b8b80bd959013cb299
bMeasel <49103411+bMeasel@users.noreply.github.com>
2019-03-30 16:07:36 -0400

    Methods

--- CodeConverter/src/CodeConverterDriver.java
+++ CodeConverter/src/CodeConverterDriver.java
@@ -5,5 +5,21 @@ public class CodeConverterDriver {
 	{
 		
 	}
-
+	public static String ConvertSelector(int indexFrom, int indexTo, String code) {
+		if(indexFrom == indexTo) {
+			return code;
+		}
+		else if(indexFrom == 0){
+			return JavaToPython(code);
+		}
+		else{
+			return PythonToJava(code);
+		}
+	}
+	public static String JavaToPython(String orig) {
+		return orig;
+	}
+	public static String PythonToJava(String orig) {
+		return orig;
+	}
 }
