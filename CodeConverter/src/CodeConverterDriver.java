c2a4dd9e2ddefcd057bcaa631e8f3bbe6c37527b
bMeasel <49103411+bMeasel@users.noreply.github.com>
2019-03-30 17:30:16 -0400

    Update CodeConverterDriver.java

--- CodeConverter/src/CodeConverterDriver.java
+++ CodeConverter/src/CodeConverterDriver.java
@@ -35,24 +35,52 @@ public class CodeConverterDriver {
 		}
 	}
 	
+	
 	public static String JavaCPPLineBreakdown(String orig)
 	{
+		String[] res = orig.split(" ");
 		
 	}
+	
 	
 	public static String CPPToJava(String orig) 
 	{
 		String[] res = orig.split(";");
 		String after;
+		//test for C++ using system input
+		int num = orig.indexOf('>');
+		char ch = ' ';
+		char b = ' ';
+		
 		for(int i = 0; i < res.length; i++) 
 		{
-			after = res[i];
-			res[i] = CPPJavaLineBreakdown(after) + ";";
+			if(num >=0)
+			{
+				ch = orig.charAt(num+1);
+				b = orig.charAt(num);
+			}
+			if((num >= 0) && (ch == b) && (i == 0))
+			{
+				after = res[i];
+				res[i] = "import java.util.Scanner; " + CPPJavaLineBreakdown(after) + ";";
+			}
+			else 
+			{
+				after = res[i];
+				res[i] = CPPJavaLineBreakdown(after) + ";";
+			}
 		}
 	}
 	
+	
 	public static String CPPJavaLineBreakdown(String orig) 
 	{
-		
+		if((orig.indexOf("cout <<") >= 0) || (orig.indexOf("cout<<") >= 0))
+		{
+			if((orig.indexOf("<< endl") >= 0) || (orig.indexOf("<<endl") >= 0))
+			{
+				
+			}
+		}
 	}
 }
